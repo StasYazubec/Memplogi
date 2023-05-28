92a742cebec073aa5f19bb1900a35302d9e1444b
AdolfEnjoer <128312157+AdolfEnjoer@users.noreply.github.com>
2023-05-28 20:10:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let botton=document.querySelector(".botton")
+let hrefs=["./mempage","./mempage1","./mempage2","./mempage3","./mempage4"]
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+function random_mem(){
+    let a=getRandomInt(hrefs.lenght)
+    window.location.href = hrefs[a]
+
+}
+botton.addEventListener("click",random_mem)
