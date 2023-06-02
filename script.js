37146d27804b66d4b3da4781a783d7eba4026cc3
StasYazubec <103126435+StasYazubec@users.noreply.github.com>
2023-06-02 14:57:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.addEventListener('load', (event) => { 
     let botton=document.querySelector(".botton")
-let hrefs=["./mempage.html","./mempage1.html","./mempage2.html","./mempage3.html","./mempage4.html"]
+let hrefs=["/mempage.html","/mempage1.html","/mempage2.html","/mempage3.html","/mempage4.html"]
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 function random_mem(){
     let a=getRandomInt(hrefs.lenght)
-    window.location.href = hrefs[a]
+    window.location.pathname = hrefs[a]
 
 }
 botton.addEventListener("click",random_mem)
